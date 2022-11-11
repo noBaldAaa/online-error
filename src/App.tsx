0f8c384021faf6664410ff2f94adec1492b4ee08
wxy <102939294+noBaldAaa@users.noreply.github.com>
2022-11-11 12:54:45 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    ["1"].find((s: any) => s.aaaa.bbbb === "1111");
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">不要秃头啊</header>
     </div>
   );
 }
